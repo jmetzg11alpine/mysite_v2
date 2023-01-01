9af90e5ed11f73c62d935b5e7927f71d026bc565
jmetzg11alpine <jmetzg11_alpine@mailfence.com>
2023-01-01 10:32:04 -0800

    some clean up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,45 +1,12 @@
-import { useRef, useState, useEffect } from 'react'
-import * as d3 from 'd3'
 import { Link } from 'react-router-dom'
 import './header.css'
 
 const Header = () => {
-  const [headerHeight, setHeaderHeight] = useState(0)
-  const [headerWidth, setHeaderWidth] = useState()
-  const svgRef = useRef()
-  d3.select(svgRef.current)
-    .select('ellipse')
-    .attr('cx', headerWidth * 0.05)
-    .attr('cy', headerHeight * 0.68)
-    .attr('rx', headerHeight * 0.55)
-    .attr('ry', headerHeight * 0.35)
-    .style('fill', '#034078')
-  d3.select(svgRef.current)
-    .select('text')
-    .attr('x', headerWidth * 0.036)
-    .attr('y', headerHeight * 0.82)
-    .text('JM')
-    .style('fill', 'white')
-    .style('anchor-text', 'middle')
-    .style('font-size', headerWidth * 0.0225)
-    .style('font-style', 'bold')
-    .style('font-family', 'Montserrat')
-
-  useEffect(() => {
-    const header = d3.select(svgRef.current)
-    setHeaderHeight(parseInt(header.style('height')))
-    setHeaderWidth(parseInt(header.style('width')))
-  }, [])
-
   return (
-    <div ref={svgRef} className='header'>
-      <div className='header-svg-container'>
-        <svg>
-          <ellipse></ellipse>
-          <text></text>
-        </svg>
+    <div className='header'>
+      <div className='header-logo-container'>
+        <p>JM</p>
       </div>
-
       <div className='header-link-container'>
         <Link to='/' className='header-link'>
           Home
--- src/components/home/components/Directory.jsx
+++ src/components/home/components/Directory.jsx
@@ -1,10 +1,10 @@
 import '../styles/directory.css'
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
-import machine_learning_img from '../imgs/machine_learning.jpeg'
+import machine_learning_img from '../imgs/machine_learning.png'
 import city_graph_img from '../imgs/city_graph.png'
 import quiz_time_img from '../imgs/quiz_time.png'
-import world_financial_img from '../imgs/world_financial.jpg'
+import world_financial_img from '../imgs/world_markets.png'
 import my_tools_img from '../imgs/my_tools.png'
 
 const Directory = () => {
@@ -13,17 +13,16 @@ const Directory = () => {
       <Carousel>
         <Carousel.Item>
           <div className='home-carosel-container'>
-            <div className='home-carosel-image'>
-              <h3>Directory</h3>
+            <div className='home-carosel-left'>
               <img className='home-carosel-img' src={machine_learning_img} alt='First slide' />
             </div>
-            <div className='home-carosel-info'>
-              <h3>
+            <div className='home-carosel-right'>
+              <h3 className='home-info-heading'>
                 <a href='https://jesse-metzger.vercel.app/ml' rel='noreferrer' target='_blank'>
                   Machine Learning
                 </a>
               </h3>
-              <p>
+              <p className='home-info-p'>
                 There are two projects here. The NLP one has micro servers that work daily to get Ney York Time articles about each of the 50 states and categoriezes each article. It categorizes each artice using the famous BART model. <br></br>
                 <br></br>
                 The second project is an image recogniztion model that was created with tensorflow. The training data is me drawing each number 100 times. It doesn't work well with everyone and that is why I added a feature where users can help grow my training data with more diverse numbers.
@@ -33,71 +32,65 @@ const Directory = () => {
         </Carousel.Item>
         <Carousel.Item>
           <div className='home-carosel-container'>
-            <div className='home-carosel-image'>
-              <h3>Directory</h3>
-              <img className='home-carosel-img' src={city_graph_img} alt='Second slide' />
-            </div>
-            <div className='home-carosel-info'>
-              <h3>
+            <div className='home-carosel-left'>
+              <h3 className='home-info-heading'>
                 <a href='https://jesse-metzger.vercel.app/livability' rel='noreferrer' target='_blank'>
                   Interactive Graphic with City Data
                 </a>
               </h3>
-              <p>
+              <p className='home-info-p'>
                 These graphics where made with D3.js and such a strategy could be used to compare the features of different data samples. In my case I used the 50 biggest US cities and 9 different features that I scoured the web for. The left 3 graphs show the features of a selected city that most closely fit the distribution of the other cities. The right three graphs show how the selected city
                 most differes from the other cities. The feature rankings where determined by z-scores, and much care was put into the pop up text when hovering the mouse over a data point.
               </p>
             </div>
+            <div className='home-carosel-right'>
+              <img className='home-carosel-img' src={city_graph_img} alt='Second slide' />
+            </div>
           </div>
         </Carousel.Item>
         <Carousel.Item>
           <div className='home-carosel-container'>
-            <div className='home-carosel-image'>
-              <h3>Directory</h3>
+            <div className='home-carosel-left'>
               <img className='home-carosel-img' src={quiz_time_img} alt='Third slide' />
             </div>
-
-            <div className='home-carosel-info'>
-              <h3>
+            <div className='home-carosel-right'>
+              <h3 className='home-info-heading'>
                 <a href='https://jesse-metzger.vercel.app/quiz' rel='noreferrer' target='_blank'>
                   4 Quizzes
                 </a>
               </h3>
-              <p>These are 4 quizzes where the scores are recorded in a database. The D3.js bar charts update automatically when new scores are entered. Only people with a login and password can take the quiz. Don't be shy and ask for credentials.</p>
+              <p className='home-info-p'>These are 4 quizzes where the scores are recorded in a database. The D3.js bar charts update automatically when new scores are entered. Only people with a login and password can take the quiz. Don't be shy and ask for credentials.</p>
             </div>
           </div>
         </Carousel.Item>
         <Carousel.Item>
           <div className='home-carosel-container'>
-            <div className='home-carosel-image'>
-              <h3>Directory</h3>
-              <img className='home-carosel-img' src={world_financial_img} alt='Third slide' />
-            </div>
-
-            <div className='home-carosel-info'>
-              <h3>
+            <div className='home-carosel-left'>
+              <h3 className='home-info-heading'>
                 <a href='https://jesse-metzger.vercel.app/world' rel='noreferrer' target='_blank'>
                   My investment and the world markets
                 </a>
               </h3>
-              <p>My investments are visualized through D3.js graphs</p>
-              <p>There is also a world map that get data from scrapping a website. It shows how various national stock markets have changed in the past week.</p>
+              <p className='home-info-p'>My investments are visualized through D3.js graphs</p>
+              <p className='home-info-p'>There is also a world map that get data from scrapping a website. It shows how various national stock markets have changed in the past week.</p>
+            </div>
+            <div className='home-carosel-right'>
+              <img className='home-carosel-img' src={world_financial_img} alt='Third slide' />
             </div>
           </div>
         </Carousel.Item>
         <Carousel.Item>
           <div className='home-carosel-container'>
-            <div className='home-carosel-image'>
-              <h3>Directory</h3>
+            <div className='home-carosel-left'>
               <img className='home-carosel-img' src={my_tools_img} alt='Third slide' />
             </div>
-            <div className='home-carosel-info'>
-              <h3>
+            <div className='home-carosel-right'>
+              <h3 className='home-info-heading'>
                 <a href='https://jesse-metzger.vercel.app/projects' rel='noreferrer' target='_blank'>
                   Data Science Projects
                 </a>
               </h3>
-              <p>These are various projects that I did while I was just learning Python and I had just finished graduate school. It was from these individual projects that I gained the skills to break into the Data Science sphere.</p>
+              <p className='home-info-p'>These are various projects that I did while I was just learning Python and I had just finished graduate school. It was from these individual projects that I gained the skills to break into the Data Science sphere.</p>
             </div>
           </div>
         </Carousel.Item>
