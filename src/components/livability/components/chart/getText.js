5d2a872d243f7350347399ddf4dd0e22c3430b8d
jmetzg11alpine <jmetzg11_alpine@mailfence.com>
2023-01-03 21:15:35 -0800

    fixed text

--- src/components/livability/components/chart/getText.js
+++ src/components/livability/components/chart/getText.js
@@ -1,47 +1,22 @@
 const getFitText = (category, city) => {
   if (category === 'income') {
-    return (
-      city +
-      "'s wealth is nothing special. It's not a rich city like San Jose, but it's not poor like Detroit"
-    )
+    return city + "'s wealth is nothing special. It's not a rich city like San Jose, but it's not poor like Detroit"
   } else if (category === 'ufo') {
-    return (
-      "Aliens don't love " +
-      city +
-      ' like they love New York, but they like ' +
-      city +
-      ' more than Oakland'
-    )
+    return "Aliens don't love " + city + ' like they love New York, but they like ' + city + ' more than Oakland'
   } else if (category === 'temperature') {
-    return (
-      'Normal temperatures in ' +
-      city +
-      '. Not hot like Houston nor cold like Milwaukee'
-    )
+    return 'Normal temperatures in ' + city + '. Not hot like Houston nor cold like Milwaukee'
   } else if (category === 'crime') {
     return city + ' is more dangerous than Arlington, but safer than Oakland'
   } else if (category === 'population') {
-    return (
-      city +
-      " isn't a rat next like New York, and isn't a yuppie village like Arlington"
-    )
+    return city + " isn't a rat next like New York, and isn't a yuppie village like Arlington"
   } else if (category === 'air') {
-    return (
-      'Fresno has worse air than ' + city + ' ,but Colorado Springs has better'
-    )
+    return 'Fresno has worse air than ' + city + ' ,but Colorado Springs has better'
   } else if (category === 'parking') {
-    return (
-      'You can find quicker parking in Omaha, but ' +
-      city +
-      ' is still quicker than New York'
-    )
+    return 'You can find quicker parking in Omaha, but ' + city + ' is still quicker than New York'
   } else if (category === 'size') {
     return city + ' is bigger than Miami, but smaller than Jacksonville'
   } else {
-    return (
-      city +
-      " doesn't have as many electric car stations as Los Angeles, but has more than Wichita"
-    )
+    return city + " doesn't have as many electric car stations as Los Angeles, but has more than Wichita"
   }
 }
 
@@ -56,17 +31,13 @@ const getNoFitText = (category, city, direction) => {
     } else if (category === 'crime') {
       return "Don't let your children play unsupervised in " + city
     } else if (category === 'population') {
-      return (
-        'The school district of ' +
-        city +
-        ' needs to introduce a class on sex education'
-      )
+      return 'The school district of ' + city + ' needs to introduce a class on sex education'
     } else if (category === 'air') {
       return "It's healthier to smoke cigarettes than to live in " + city
     } else if (category === 'parking') {
       return 'Lots of time is spent trying to find parking in ' + city
     } else if (category === 'size') {
-      return city + ' has lots of extra land for big Americans'
+      return city + ' has lots of extra land for big American dreams'
     } else {
       return city + ' has lots places to charge your electric car'
     }
@@ -80,11 +51,7 @@ const getNoFitText = (category, city, direction) => {
     } else if (category === 'crime') {
       return "You don't have to lock your doors in " + city
     } else if (category === 'population') {
-      return (
-        'The mayor of ' +
-        city +
-        ' should consider programs to encourage more romance'
-      )
+      return 'The mayor of ' + city + ' should consider programs to encourage more romance'
     } else if (category === 'air') {
       return city + ' is a breath of fresh air'
     } else if (category === 'parking') {
@@ -92,9 +59,7 @@ const getNoFitText = (category, city, direction) => {
     } else if (category === 'size') {
       return 'Not much room to build houses in ' + city
     } else {
-      return (
-        city + " doesn't support electric cars. Not many places to charge them"
-      )
+      return city + " doesn't support electric cars. Not many places to charge them"
     }
   }
 }
